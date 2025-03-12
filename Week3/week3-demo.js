2c91d415ac7e91f551778258d1a2ebe0c2e2b126
Caleb Pham <cp42289@my.utexas.edu>
2025-03-11 20:23:40 -0500

    week 3

--- Week3/week3-demo.js
+++ Week3/week3-demo.js
@@ -4,3 +4,33 @@ Task: write a function, walkToTreasure, that takes in x and y coordinates on a b
 and “walks” to the location of the treasure buried in the sand. 
 */
 
+walkToTreasure(0,0);
+
+function walkToTreasure(x, y) {
+    var treasureX = 3;
+    var treasureY = 9;
+
+    // find x
+    while (x != treasureX) {
+        if (x < treasureX) {
+            x++;
+            console.log("Moving right");
+        } else {
+            x--;
+            console.log("Moving left");
+        }
+    }
+
+    // find y
+    while (y != treasureY) {
+        if (y < treasureY) {
+            y++;
+            console.log("Moving up");
+        } else {
+            y--;
+            console.log("Moving down");
+        }
+    }
+
+    console.log("Found Treasure");
+}
